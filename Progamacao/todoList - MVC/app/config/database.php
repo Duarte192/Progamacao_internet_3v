552007562fddfe965ea7a906f61060479bf5d48f
Gabriel Pereira Duarte <20241134010027@ifrn.local>
2026-09-18 16:03:56 -0300

    a

--- Progamacao/todoList - MVC/app/config/database.php
+++ Progamacao/todoList - MVC/app/config/database.php
@@ -1,21 +1,22 @@
 <?php
 
-class Database{
-    private $host = 'localhost';
-    private $user = 'root';
-    private $password = '';
-    private $banco = 'todo_list';
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $senha = "";
+    private $banco = "todo_list";
 
     public $conn = '';
 
     public function conectar(){
-        $this -> conn = new mysqli($this -> host, $this -> user, $this -> password, $this -> banco );
+        $this->conn = new mysqli($this->host, $this->username, $this->senha, $this->banco);
 
-        if ($this -> conn -> connect_error) {
-            die("Algo deu errado" . $this -> conn -> connect_error);
+        if ($this->conn->connect_error) {
+            die("algo deu errado".$this->conn->connect_error);
         }
 
         return $this->conn;
     }
-
 }
+
+?>
